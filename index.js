86dce9ece0097c442c1d228f2e2b53d573b725a9
david-chua <dp_chua@hotmail.com>
2018-03-06 03:47:45 -0500

    started designing contact

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var logger = require('morgan');
+
+
+app.use(logger('dev'));
 
 app.use(express.static(__dirname + '/views'));
 
 app.use(express.static(__dirname + '/script'));
+
+app.use(express.static(__dirname + '/files'));
 
 
 app.get('/skills', function(req,res){
@@ -19,13 +25,25 @@ app.get('/projects', function(req,res){
   res.sendFile(__dirname + '/views' + '/projects.html');
 });
 
+app.get('/resume', function(req,res){
+  res.sendFile(__dirname + '/views' + '/resume.html');
+});
+
+app.get('/about', function(req,res){
+  res.sendFile(__dirname + '/views' + '/about.html');
+});
+
+app.get('/contact', function(req,res){
+  res.sendFile(__dirname + '/views' + '/contact.html');
+});
+
+app.get('/success', function(req,res){
+  res.sendFile(__dirname + '/views' + '/success.html');
+});
 
 app.get('/', function(req,res){
   res.sendFile('/index.html');
 });
-
-
-
 
 app.listen(process.env.PORT || 3000, function(){
   console.log('working portfolio');
