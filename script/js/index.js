2dd7fe069213c6c0fddd64f828d3fe705058e9c3
david-chua <dp_chua@hotmail.com>
2018-04-26 02:16:34 -0400

    finished 5 web projects in portfolio

--- script/js/index.js
+++ script/js/index.js
@@ -21,12 +21,34 @@ $(function() {
         $( "#trainingForGainsWebInfo" ).toggle();
     });
 });
-
+// Toggle Mac View More Button for EastC Fashion
 $(function() {
     $( "#eastfashionButton" ).click(function() {
         $( "#eastcFashionInfo" ).toggle();
     });
 });
+
+// Toggle Mac View More Button for myReads
+$(function() {
+    $( "#myReadsButton" ).click(function() {
+        $( "#myReadsInfo" ).toggle();
+    });
+});
+
+// Toggle Mac View More Button for Topshelf Imagination
+$(function() {
+    $( "#topshelfButton" ).click(function() {
+        $( "#topshelfInfo" ).toggle();
+    });
+});
+
+// Toggle Mac View More Button for What's Cooking
+$(function() {
+    $( "#cookingButton" ).click(function() {
+        $( "#cookingInfo" ).toggle();
+    });
+});
+
 
 // Close Information Button
 //Close Training for Gains Info
@@ -39,5 +61,26 @@ $(function(){
 $(function(){
   $(".close_fashion_button").click(function(){
     $("#eastcFashionInfo").toggle();
+  });
+});
+
+//Close My Reads Information
+$(function(){
+  $(".close_myReads_button").click(function(){
+    $("#myReadsInfo").toggle();
+  });
+});
+
+//Close Topshelf  Information
+$(function(){
+  $(".close_topshelf_button").click(function(){
+    $("#topshelfInfo").toggle();
+  });
+});
+
+//Close Cooking Information
+$(function(){
+  $(".close_cooking_button").click(function(){
+    $("#cookingInfo").toggle();
   });
 });
